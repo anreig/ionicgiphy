5efe8353b47f7db079dc49dfe32ef8a4187e81e0
Angel Reig <anreig@gmail.com>
2019-09-11 23:41:28 +0200

    https api url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  API_URL: 'http://api.giphy.com/v1/gifs/',
+  API_URL: 'https://api.giphy.com/v1/gifs/',
   API_KEY: '7MCKuTbHYGvSeAT2MNoEV3XhFnGHzCF8'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  API_URL: 'http://api.giphy.com/v1/gifs/',
+  API_URL: 'https://api.giphy.com/v1/gifs/',
   API_KEY: '7MCKuTbHYGvSeAT2MNoEV3XhFnGHzCF8'
 };
 
